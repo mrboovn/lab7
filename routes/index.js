af3879391dec8bc9fc77fe9694797afe83fe536d
Minh Nguyen <m9nguyen@ucsd.edu>
2021-02-18 01:39:40 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var projects = require('../projects.json');
+vvar projects = require('../projects.json');
 
 /*
  * GET home page.
  */
 
 exports.view = function(request, response){
+  	
+  	projects['viewAlt'] = false;
   	response.render('index', projects);
+  	
 };
+
+exports.viewAlt = function(request, response){
+	
+	projects['viewAlt'] = true;
+	response.render('index', projects);
+}
